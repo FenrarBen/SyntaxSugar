1ae842fe119fe308898a368a13333971a0c9fdd7
BenStrawbridge <lute_midnight@yahoo.com>
2020-10-27 11:44:23 -0500

    Syntax Sugar Exercise

---
+++ Exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
